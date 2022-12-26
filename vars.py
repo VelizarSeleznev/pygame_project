54a3ac1091d9a899d7d97bd1d230d15d7217093d
Velizar <velizar.seleznev@gmail.com>
2022-12-26 13:18:21 +0100

    Немного изменено положение переменных (глобальные/не глобальные)

--- vars.py
+++ vars.py
@@ -2,9 +2,9 @@
 import os
 import sys
 import json
-import random
 
 import logging
+import configparser
 
 import pygame
 import pygame_widgets
@@ -15,23 +15,21 @@ from abc import ABC, abstractmethod
 from spritesheet import Spritesheet
 
 # GLOBAL:
+cfg = configparser.ConfigParser()
+cfg.read("settings.ini")
 g = None  # (Game) game unit
 corSceneNum = 0  # изначально 0 - главное меню
 corLevel = None  # (Level) current game level
 vec = p.math.Vector2
-width, height = 1920, 800
-
+fps = int(cfg["screen"]["fps"])
+width, height = int(cfg["screen"]["width"]), int(cfg["screen"]["height"])
+block_width, block_height = 150, 150  # размеры одной клетки
+title = cfg["screen"]["title"]
 
 class Vars:
     def __init__(self):
-        self.camera = None
-        self.fps = 60
-        self.width, self.height = width, height
-        self.title = 'my_title'
         self.sc = None  # window
         self.corSceneNum = 0  # current scene number
-        self.player = None
         self.joysticks = {}
-
         self.prog_running = True  # running program (scene switching loop)
         self.loop_running = True  # running render/logic loop
